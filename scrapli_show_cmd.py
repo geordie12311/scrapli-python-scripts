d443363580699a480bc1739dbe7b5f7b08db400a
geordie12311 <georgeharwood@live.co.uk>
2022-12-04 13:41:27 +0000

    new scripts

--- scrapli_show_cmd.py
+++ scrapli_show_cmd.py
@@ -1,20 +1,39 @@
 """
-Simple scrapli script using Cisco sandbox device
+Simple scrapli script to connect to multiple hosts
+This example uses connection manager to manage,
+opening and closing the connection
 """
 
 from scrapli.driver.core import IOSXEDriver #importing IOSXEDriver from scrapli
 
-my_device = {
-    "host": "sandbox-iosxe-latest-1.cisco.com",
-    "auth_username": "developer",
-    "auth_password": "C1sco12345",
-    "auth_strict_key": False,
-}
-# above, creating an object called my_device to hold the hostname and credentials that scrapli will use to connect
+devices = [
+    {
+        "host": "vIOS1",
+        "auth_username": "cisco",
+        "auth_password": "cisco123",
+        "auth_strict_key": False,
+        "ssh_config_file": True,
+    },
+    {
+        "host": "vIOS2",
+        "auth_username": "cisco",
+        "auth_password": "cisco123",
+        "auth_strict_key": False,
+        "ssh_config_file": True,
+    },
+    {
+        "host": "vIOS3",
+        "auth_username": "cisco",
+        "auth_password": "cisco123",
+        "auth_strict_key": False,
+        "ssh_config_file": True,
+    },
+]
+# above, creating an object called devices to hold the hostnames and credentials that scrapli will use to connect
 
-conn = IOSXEDriver(**my_device) # creating an object called conn, linking to IOSXEDriver and using my_device creds
-conn.open() #opening the connection to my_device
-response = conn.send_command("Show IP interface brief") #creating the object response and using send_command to send command to host
-
+for device in devices:
+    with IOSXEDriver(**device) as conn: #using a for loop with content manager to connect to the hosts
+        response = conn.send_command("Show IP interface brief") #creating the object response and using send_command to send command to hosts
+    
 print(response.result) #printing the response results to screen
-conn.close() #closing the connection to the host
+#using with to create the content manager to simplify connecting to the hoste
